c108c23e96bc0905f80fbc354829b4e5a0c7fd8c
yukihop <mobacure.black@gmail.com>
2015-09-26 03:46:42 +0900

    Google analytics

--- main.js
+++ main.js
@@ -125,3 +125,13 @@ function save() {
 	var data = makeSaveData();
 	localStorage.setItem('clearData', JSON.stringify(data));
 }
+
+// Google analytics
+if (location.href.match(/github/)) {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-68109293-1', 'auto');
+	ga('send', 'pageview');
+}
