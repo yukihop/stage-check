4f72786879721fabcede9526a9017c810261c892
yukihop <mobacure.black@gmail.com>
2016-03-01 18:21:06 +0900

    Comment

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
+
+# Manual deploy script to compile everything with 'production' mode
+# and pushes the compiled material to the gh-pages branch.
+# intended to be used only by the project owner.
+
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
+# Clear and re-create the output directory
 rm -rf built || exit 0;
 mkdir built;
 
-# run our compile script, discussed above
+# Run our compile script
 NODE_ENV=production webpack
 
-# go to the out directory and create a *new* Git repo
+# Go to the built directory and create a *new* Git repo
 cd built
-git init
 
-# inside this git repo we'll pretend to be a new user
+git init
 git config user.name "yukihop"
 git config user.email "mobacure.black@gmail.com"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+# Make the first and only commit to this new Git repo
 git add .
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+# repo's gh-pages branch. (All previous history on the gh-pages branch will be lost.)
 git push --force "https://yukihop@github.com/yukihop/stage-check.git" master:gh-pages
+
+# we can remove the .git directory at this point.
