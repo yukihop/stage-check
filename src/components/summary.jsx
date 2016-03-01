b749bc03881cba400bf24e6640592f2afce48904
yukihop <mobacure.black@gmail.com>
2016-03-01 14:19:50 +0900

    Show total in summary

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -3,14 +3,21 @@ import { difficulty, difficultyOrder } from '../difficulty';
 import Well from 'react-bootstrap/lib/Well';
 
 export let Summary = props => {
+	let totalCheckable = 0;
+	let totalChecked = 0;
+
 	let items = difficultyOrder.map(dif => {
 		let difStr = difficulty[dif][1];
 		let checked = props.tunes.filter(
 			tune => props.save[tune.id] && props.save[tune.id][dif]
 		).length;
+		totalChecked += checked;
 		let checkable = props.tunes.length;
+		totalCheckable += checkable;
 		return `${difStr} ${checked}/${checkable}`;
 	});
+
+	items.push(`Total ${totalChecked}/${totalCheckable}`);
 
 	return <Well className="summary" bsSize="sm">
 		{items.join('; ')}
